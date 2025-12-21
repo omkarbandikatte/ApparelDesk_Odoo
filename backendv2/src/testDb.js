5d0af8c141ec6c7ff9564de68663436ef7013053
Omkar Bandikatte <149424461+omkarbandikatte@users.noreply.github.com>
2025-12-21 09:42:01 +0530

    Delete backendv2 directory

--- backendv2/src/testDb.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const pool = require("./config/db");
-
-(async () => {
-  const result = await pool.query("SELECT NOW()");
-  console.log(result.rows);
-  process.exit(0);
-})();
